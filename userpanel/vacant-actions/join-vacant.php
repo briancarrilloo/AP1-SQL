8609411eb19795a797df0534c4c2587077cc6996
brian <bcarrillo2302@gmail.com>
2021-02-04 15:43:20 +0100

    Alerta de join-vacant

--- userpanel/vacant-actions/join-vacant.php
+++ userpanel/vacant-actions/join-vacant.php
@@ -9,12 +9,21 @@
         fclose($myFile);
     }
 
-    //Añadir registros
+    // Añadir registros
     $line = "El usuario " . $_SESSION['user'] . ", se ha registrado candidato para la vacante " . $_GET['id'] . "\n";
     
     $myFile = fopen("../../logs/RegistroVacantes.txt", "a") or die ("El archivo no se ha podido abrir.");
     fwrite($myFile, $line);
     fclose($myFile);
 
-
+    //Mostrar alerta de registro correcto
+    echo '<script language="javascript">alert("Te has registrado para la vacante");</script>';
+    switch($_SESSION['role']){
+        case 1:
+            echo "<script>document.location = '../panel-alumn.php'</script>";
+            break;
+        case 2:
+            echo "<script>document.location = '../panel-exalumn.php'</script>";
+            break;
+    }
 ?>
